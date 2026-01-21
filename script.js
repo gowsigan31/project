508b0cd9d896832fc2a22af592fadf179de8b4ce
gowsigan31 <gowsigan31@hotmail.com>
2026-01-20 23:08:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,6 @@ L.marker(torontoCoords).addTo(map)
     .bindPopup('A center point in the City of Toronto.')
     .openPopup();
 
-// config map
-const config = {
-  minZoom: 7,
-  maxZoom: 18,
-  // https://github.com/Leaflet/Leaflet.fullscreen
-  fullscreenControl: true,
-};
-
 // create legend
 const legend = L.control({ position: "bottomleft" });
 
@@ -75,3 +67,73 @@ L.control
     imperial: false,
   })
   .addTo(map);
+
+// sidebar
+
+const menuItems = document.querySelectorAll(".menu-item");
+const sidebar = document.querySelector(".sidebar");
+const buttonClose = document.querySelector(".close-button");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (
+      target.classList.contains("active-item") ||
+      !document.querySelector(".active-sidebar")
+    ) {
+      document.body.classList.toggle("active-sidebar");
+    }
+
+    // show content
+    showContent(target.dataset.item);
+    // add active class to menu item
+    addRemoveActiveItem(target, "active-item");
+  });
+});
+
+// close sidebar when click on close button
+buttonClose.addEventListener("click", () => {
+  closeSidebar();
+});
+
+// remove active class from menu item and content
+function addRemoveActiveItem(target, className) {
+  const element = document.querySelector(`.${className}`);
+  target.classList.add(className);
+  if (!element) return;
+  element.classList.remove(className);
+}
+
+// show specific content
+function showContent(dataContent) {
+  const idItem = document.querySelector(`#${dataContent}`);
+  addRemoveActiveItem(idItem, "active-content");
+}
+
+// --------------------------------------------------
+// close when click esc
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeSidebar();
+  }
+});
+
+// close sidebar when click outside
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".sidebar")) {
+    closeSidebar();
+  }
+});
+
+// --------------------------------------------------
+// close sidebar
+
+function closeSidebar() {
+  document.body.classList.remove("active-sidebar");
+  const element = document.querySelector(".active-item");
+  const activeContent = document.querySelector(".active-content");
+  if (!element) return;
+  element.classList.remove("active-item");
+  activeContent.classList.remove("active-content");
+}
